1b5fc409a63e2eb733f3751d39336c581c60cf63
MonarchRyuzaki <shivamganguly2004@gmail.com>
2026-01-16 21:14:51 +0530

    section 2: loans and cards microservice done

---
+++ cards/src/main/java/com/eazybytes/cards/mapper/CardsMapper.java
@@ -0,0 +1,26 @@
+package com.eazybytes.cards.mapper;
+
+import com.eazybytes.cards.dto.CardsDTO;
+import com.eazybytes.cards.entity.Cards;
+
+public class CardsMapper {
+    public static CardsDTO mapToCardsDTO(Cards cards, CardsDTO cardsDTO) {
+        cardsDTO.setCardNumber(cards.getCardNumber());
+        cardsDTO.setCardType(cards.getCardType());
+        cardsDTO.setMobileNumber(cards.getMobileNumber());
+        cardsDTO.setTotalLimit(cards.getTotalLimit());
+        cardsDTO.setAvailableAmount(cards.getAvailableAmount());
+        cardsDTO.setAvailableAmount(cards.getAvailableAmount());
+        return cardsDTO;
+    }
+
+    public static Cards mapToCards(CardsDTO cardsDTO, Cards cards) {
+        cards.setCardNumber(cardsDTO.getCardNumber());
+        cards.setCardType(cardsDTO.getCardType());
+        cards.setMobileNumber(cardsDTO.getMobileNumber());
+        cards.setTotalLimit(cardsDTO.getTotalLimit());
+        cards.setAvailableAmount(cardsDTO.getAvailableAmount());
+        cards.setAvailableAmount(cardsDTO.getAvailableAmount());
+        return cards;
+    }
+}
